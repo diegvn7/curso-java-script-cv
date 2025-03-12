6467dce7c072f2413ce9e8231b9820262c3b8593
diegvn7 <diegovn7@gmail.com>
2025-03-12 12:14:08 -0300

    Create ex011.js

---
+++ exercicios/ex011/ex011.js
@@ -0,0 +1,9 @@
+var idade = 18
+
+if (idade < 16) {
+    console.log (`Não pode votar`)
+} else if (idade < 18 || idade > 70) {
+    console.log (`Voto opcional`)
+} else {
+    console.log (`Voto OBRIGAÓRIO`)
+}
