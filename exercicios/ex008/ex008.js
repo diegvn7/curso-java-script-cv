cab06d89365d0282f136a67a8446358b90be5314
diegvn7 <diegovn7@gmail.com>
2025-03-12 09:27:19 -0300

    Create ex008.js

---
+++ exercicios/ex008/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70.5
+console.log(`A velocidade so seu carro é ${vel}Km/h`)
+if (vel >= 60) {
+    console.log(`Voce ultrapassou a velocidade permitida, voce foi multado`)
+}
+console.log(`Use cinto de segurança`)
