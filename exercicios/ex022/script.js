14a5388d6812f82662b84d1488f4667407a1c868
diegvn7 <diegovn7@gmail.com>
2025-03-17 12:37:44 -0300

    Update script.js

--- exercicios/ex022/script.js
+++ exercicios/ex022/script.js
@@ -1,16 +1,57 @@
-function add() {
-    let njs = window.document.getElementById('ntxt')
-    let n = Number(njs.value)
-    let list = []
-    if (list.indexOf(n) != -1 || n < 100) {
-        list.push(n)
-        let tab = window.document.getElementById('resulttxt')
-        var item = window.document.createElement('option')
-        item.text = `O valor ${n} foi adicionado.`
-        tab.appendChild(item)
-        
+let res = window.document.getElementById('res')
+let tab = window.document.getElementById('resulttxt')
+let n = window.document.getElementById('ntxt')
+let list = []
+
+function isNum(num) {
+    if (num >= 1 && num <= 100){
+        return true
     } else {
-        window.alert('Digite um valor acima de 100 ou não repitido')
+        return false
+    }
+}
+
+function isList(num, l) {
+    if (l.indexOf(num) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+function add() { 
+    if (isNum(n.value) && !isList(n.value, list)) {
+        list.push(n.value)
+        var item = window.document.createElement('option')
+        item.text = `O valor ${n.value} foi adicionado.`
+        tab.appendChild(item)
+
+        // res.innerHTML = ''
+    } else {
+        window.alert('Digite um valor acima de 0 a 100 ou não repitido')
+    }
+
+    n.value = ''
+    n.focus()
+}
+
+function finalizar() {
+    if (list.length == 0) {
+        window.alert('Adicione um valor')
+    } else {
+        let maior = Math.max.apply(null, list)
+        let menor = Math.min.apply(null, list)
+        let soma = 0
+        for (let c = 0; c <= list.length; c++) {
+            soma += list[c]
+        }
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo são ${list.length} numeros cadastrados</p>` 
+        res.innerHTML += `<p>O Maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>A soma de todos valores foi ${soma}</p>`
     }
 }
     
